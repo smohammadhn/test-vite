c3eceaf9ae928e636de40610621bc7c9122cffa0
Mohsen <89536986+mohsnrj@users.noreply.github.com>
2023-01-13 16:40:51 +0330

    add

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 import { formalAnalyzer } from './scripts/formalAnalyzer'
+import { asyncReadFile } from './scripts/playground'
 
-let formalWords = [
-  'میتوانم',
-  'است',
-  'می‌باشد',
-  'میرود',
-  'برویم',
-  'میتواند',
-  'میخواهد',
-]
-
+// let formalWords = [
+//   'میتوانم',
+//   'است',
+//   'می‌باشد',
+//   'میرود',
+//   'برویم',
+//   'میتواند',
+//   'میخواهد',
+// ]
+const Words = asyncReadFile('./WordsData/words.txt')
+var FormalWords
+Words.then((result) => {
+  FormalWords = result
+})
+console.log(FormalWords)
 let word = 'میشه'
 
 console.log('result:>> ', formalAnalyzer(word, formalWords))
