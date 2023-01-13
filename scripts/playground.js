296934b5fefc01958c9fb2e82f389755cb48ef7d
S.Mohammad Hosseininejad <smohammadhnd@gmail.com>
2023-01-13 17:21:27 +0330

    refactor asyncReadFile method

--- scripts/playground.js
+++ scripts/playground.js
@@ -1,18 +1,6 @@
 import fs from 'vite-plugin-fs/browser'
 
 export async function asyncReadFile(filename) {
-  try {
-    const contents = fs.readFile(filename, 'utf-8')
-    const arr = await contents.then(function (result) {
-      return result.toString().split('\r\n')
-    })
-
-    // console.log(arr); // ['One', 'Two', 'Three', 'Four']
-
-    return arr
-  } catch (err) {
-    console.log(err)
-  }
+  const contents = fs.readFile(filename, 'utf-8')
+  return await contents.then((text) => text.toString().split('\n'))
 }
-
-// asyncReadFile("./sorted.txt");
